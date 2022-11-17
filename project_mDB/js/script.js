f9b2713915fffffbde0c36123484ae705bb4abdb
Vladis <vladkouznecov@gmail.com>
2022-11-17 16:37:17 +0200

    first commit from HONOR

---
+++ project_mDB/js/script.js
@@ -0,0 +1,71 @@
+
+'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
+    
+    const addBlock = document.querySelectorAll('.promo__adv img'),
+        poster = document.querySelector('.promo__bg'),
+        genre = poster.querySelector('.promo__genre'),
+        movieList = document.querySelector('.promo__interactive-list'),
+        addForm = document.querySelector('form.add'),
+        addInput = addForm.querySelector('.adding__input'),
+        checkbox = addForm.querySelector('[type="checkbox"]');
+
+        addForm.addEventListener('submit', (event) => {
+            event.preventDefault();
+            let newFilm = addInput.value;
+            const favorite = checkbox.checked;
+
+            if (newFilm) {
+                if (newFilm.length > 21) {
+                    newFilm = `${newFilm.substring(0, 22)}...`;
+                }
+                movieDB.movies.push(newFilm);
+                sortArr(movieDB.movies);
+                createML(movieDB.movies, movieList);    
+            }
+        
+            event.target.reset();
+        });
+
+
+    const deleteAdv = (arr) => {
+        arr.forEach(item => {
+            item.remove();
+        });
+    };
+    
+    genre.textContent = 'Драма';
+    poster.style.backgroundImage = 'url("./img/bg.jpg")';
+    
+    const sortArr = (arr) => {
+        arr.sort();
+    };
+    
+    function createML (films, parent){
+        parent.innerHTML = "";
+        films.forEach((film, i) => {
+            parent.innerHTML += `
+            <li class="promo__interactive-item">${i+1} ${film}
+                <div class="delete"></div>
+            </li>
+            `;
+        });
+    }
+
+
+
+    deleteAdv(addBlock);
+    sortArr(movieDB.movies);
+    createML(movieDB.movies, movieList);
+
+});
